450ca3a0fdd1d70ce33c5526561326dd608af2f0
Ravi Kumar Yadav <ravi27777777@gmail.com>
2024-04-17 02:44:12 +0530

    multer cloudinary added

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,10 @@ const authRoute = require("./routes/authRoute.js");
 const postRoute = require("./routes/postRoute.js");
 const userRoute = require("./routes/userRoute.js");
 const cors = require("cors");
+const upload = require("./multer");
+const cloudinary = require("./cloudinary"); 
+const bodyParser = require("body-parser"); 
+const fs = require("fs"); 
 
 const port = process.env.PORT;
 app.use(express.json());
@@ -19,6 +23,40 @@ app.use("/api/user",userRoute);
 app.get("/", async (req,res ) => {
     res.send("HELLO WORLD !!! ");
 })
+
+////////////////////////////////////////////////////
+
+app.use('/upload-images',upload.array('image'),async(req,res)=>{
+    const uploader=async(path)=>await cloudinary.uploads(path,'Image')
+    if(req.method==='POST')
+    {
+        const urls=[]
+        const files=req.files
+
+        for(const file of files){
+            const {path}=file 
+            const newPath=await uploader(path)
+            urls.push(newPath)
+            fs.unlinkSync(path)
+        }
+        res.status(200).json({
+            message:'Image Uploaded Successfully',
+            data:urls
+        })
+         
+    }
+    else{
+        res.status(405).json({
+            err:"Image not uploaded"
+        })
+    }
+})
+
+
+
+  
+
+////////////////////////////////////////////////////
 
 
 
