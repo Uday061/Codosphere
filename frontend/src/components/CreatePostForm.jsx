288231ce7b906969f132f0e7453d57ad4d28619f
aryan-lem <mishraaryan960@gmail.com>
2024-05-25 15:34:10 +0530

    post Form

--- frontend/src/components/CreatePostForm.jsx
+++ frontend/src/components/CreatePostForm.jsx
@@ -1,8 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react'
+import { useSelector } from 'react-redux';
+import axios from 'axios';
 
 const CreatePostForm = () => {
-    const [isOpen, setIsOpen] = useState(false);
 
+    const user = useSelector((state) => state.auth.user);
+    const token = localStorage.getItem('token');
+
+    const [isOpen, setIsOpen] = useState(false);
+    const [inputs,setInputs] = useState({ desc: "", picPath: "" })
+    
     const openModal = () => {
         setIsOpen(true);
     };
@@ -11,6 +18,29 @@ const CreatePostForm = () => {
         setIsOpen(false);
     };
 
+    const change = (e) => {
+        const { name, value } = e.target;
+        setInputs({ ...inputs, [name]: value });
+    }
+
+    const handlePost=async()=>{
+        try {
+            const url = `http://localhost:5555/api/post/create`;
+            const response = await axios.post(url, {
+                userId: user._id,
+                description: inputs.desc,
+                picturePath: inputs.picPath
+            }, {
+                headers: {
+                    Authorization: `Bearer ${token}` // Attach JWT token to the request
+                }
+            });
+            setInputs({ desc: "", picPath: "" });
+            setIsOpen(false);
+        } catch (error) {
+            console.log(error);
+        }
+    }
     
     return (
         <div className="flex justify-center items-center ">
@@ -26,7 +56,7 @@ const CreatePostForm = () => {
                     <div className="bg-white w-11/12 md:w-3/4 lg:w-2/3 xl:w-1/2 p-6 rounded shadow-lg">
                         <div className="heading text-center font-bold text-2xl m-5 text-gray-800">New Post</div>
                         <div className="editor mx-auto w-full flex flex-col text-gray-800 border border-gray-300 p-4 shadow-lg">
-                            <textarea className="description bg-gray-100 sec p-3 h-60 border border-gray-300 outline-none" spellCheck="false" placeholder="Describe everything about this post here"></textarea>
+                            <textarea className="description bg-gray-100 sec p-3 h-60 border border-gray-300 outline-none" spellCheck="false" placeholder="Describe everything about this post here" value={inputs.desc} onChange={change} name='desc'></textarea>
                             <div className="icons flex text-gray-500 m-2">
                                 <svg className="mr-2 cursor-pointer hover:text-gray-700 border rounded-full p-1 h-7" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
                                 <svg className="mr-2 cursor-pointer hover:text-gray-700 border rounded-full p-1 h-7" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13" /></svg>
@@ -34,7 +64,7 @@ const CreatePostForm = () => {
                             </div>
                             <div className="buttons flex">
                                 <div onClick={closeModal} className="btn border border-gray-300 p-1 px-4 font-semibold cursor-pointer text-gray-500 ml-auto">Cancel</div>
-                                <div onClick={closeModal} className="btn border border-indigo-500 p-1 px-4 font-semibold cursor-pointer text-gray-200 ml-2 bg-indigo-500">Post</div>
+                                <div onClick={handlePost} className="btn border border-indigo-500 p-1 px-4 font-semibold cursor-pointer text-gray-200 ml-2 bg-indigo-500">Post</div>
                             </div>
                         </div>
                     </div>
