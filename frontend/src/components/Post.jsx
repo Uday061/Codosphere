d9247432f041dbef4af2c0fa8b7136ba33504dec
Ravi Kumar Yadav <ravi27777777@gmail.com>
2024-05-25 12:42:22 +0530

    likes unlikes

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -24,9 +24,9 @@ const Post = (props) => {
           });
 
           if (response.status === 200) {
-            setLikes(likes + 1);
-            setIsLiked(true);
-          }
+            setIsLiked(!isLiked);
+            setLikes(prevLikes => isLiked ? prevLikes - 1 : prevLikes + 1);
+        }
     
           
         } catch (error) {
@@ -73,19 +73,19 @@ const Post = (props) => {
                         {props.post.description}
                     </p>
                     <div className="mt-4 flex items-center">
-                        <div className="flex mr-2 text-gray-700 text-sm mr-3">
+                    <div className="flex mr-2 text-gray-700 text-sm mr-3">
                             <div style={buttonStyle} onClick={handleClickLike}>
-                                <svg fill="none" viewBox="0 0 24 24" className="w-4 h-4 mr-1" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                                <svg fill={isLiked ? 'red' : 'none'} viewBox="0 0 24 24" className="w-4 h-4 mr-1" stroke="currentColor">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
                                 </svg>
                             </div>
-                            <span>{props.post.likes.length}</span>
+                            <span>{likes}</span>
                         </div>
                         <div className="flex mr-2 text-gray-700 text-sm mr-8">
                             <svg fill="none" viewBox="0 0 24 24" className="w-4 h-4 mr-1" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z" />
                             </svg>
-                            <span>8</span>
+                            <span>{props.post.comments.length}</span>
                         </div>
                         <div className="flex mr-2 text-gray-700 text-sm mr-4">
                             <svg fill="none" viewBox="0 0 24 24" className="w-4 h-4 mr-1" stroke="currentColor">
