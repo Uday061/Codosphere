b8db1015808060c2af9f57b8a505d3af0551fa8d
Uday <udaynauhbar@Udays-MacBook-Air.local>
2024-06-04 10:08:28 +0530

    resolved third party cookie error on home page and done bit CSS

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,78 +1,160 @@
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+// import { useSelector, useDispatch } from 'react-redux'
+// import Post from '../components/Post';
+// import CreatePostForm from '../components/CreatePostForm';
+
+// const Home = () => {
+//     const token = localStorage.getItem('token');
+//     const state = useSelector((state) => state.auth);
+//     const [friends, setFriends] = useState([]);
+//     const [posts, setPosts] = useState([]);
+
+
+//     const user = useSelector((state) => state.auth.user);
+
+    
+//     {
+        
+//         useEffect(() => {
+//             const getAllPost = async () => {
+//                 for (let i = 0; i < user.friends.length; i++) {
+//                     const fid = user.friends[i];
+//                     try {
+//                         const url = `http://localhost:5555/api/post/${fid}/posts`;
+//                         const response = await axios.get(url, {
+//                             headers: {
+//                                 Authorization: `Bearer ${token}` // Attach JWT token to the request
+//                             }
+//                         });
+//                         console.log(response.data)
+
+//                         setPosts(...posts, response.data);
+//                         //console.log(posts);
+
+
+//                     } catch (err) {
+//                         console.log(err);
+
+//                     }
+//                 }
+//             }
+//             getAllPost();
+//             console.log("Here i clg posts -->", posts);
+//         }, [user]);
+//     }
+
+
+
+
+//     return (
+//         <>
+//             <>
+//                 {!user && <h2>Loading...</h2>}
+//                 {user && <h1>Welcome {user.firstName} {user.lastName}</h1>}
+
+//                 <CreatePostForm></CreatePostForm>
+
+//             </>
+//             <div>
+//                 {/* Render posts */}
+//                 {posts && posts.map(post => (
+//                     <div key={post.id} >
+//                         {/* Render each post */}
+//                         <Post post={post} />
+//                     </div>
+//                 ))}
+//                 {!friends &&
+//                     <div>
+//                         gian
+//                     </div>
+//                 }
+//             </div>
+
+//         </>
+//     );
+// };
+
+// export default Home;
+
+
+
+
+
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux';
 import Post from '../components/Post';
 import CreatePostForm from '../components/CreatePostForm';
+import { ClipLoader } from 'react-spinners';
 
 const Home = () => {
     const token = localStorage.getItem('token');
-    const state = useSelector((state) => state.auth);
-    const [friends, setFriends] = useState([]);
-    const [posts, setPosts] = useState([]);
-
-
     const user = useSelector((state) => state.auth.user);
-
-
+    const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const getAllPost = async () => {
-            for (let i = 0; i < user.friends.length; i++) {
-                const fid = user.friends[i];
-                try {
+            try {
+                setLoading(true);
+                let allPosts = [];
+                for (let i = 0; i < user.friends.length; i++) {
+                    const fid = user.friends[i];
                     const url = `http://localhost:5555/api/post/${fid}/posts`;
-                    const response = await axios.get(url,{
+                    const response = await axios.get(url, {
                         headers: {
-                          Authorization: `Bearer ${token}` // Attach JWT token to the request
+                            Authorization: `Bearer ${token}` // Attach JWT token to the request
                         }
-                      });
-                      console.log(response.data)
-
-                    setPosts(...posts, response.data);
-
-
-                } catch (err) {
-                    console.log(err);
-
+                    });
+                    allPosts = allPosts.concat(response.data);
                 }
+                setPosts(allPosts);
+                setLoading(false);
+            } catch (err) {
+                console.log(err);
+                setLoading(false);
             }
+        };
+        if (user && user.friends.length > 0) {
+            getAllPost();
+        } else {
+            setLoading(false);
         }
-        getAllPost();
-        console.log(posts)
-    }, [user]);
-
-
-
+    }, [user, token]);
 
     return (
         <>
-        <>
-            {!user && <h2>Loading...</h2>}
+            {!user && (
+                <div className="flex items-center justify-center min-h-screen">
+                    <ClipLoader color="#00008B" loading={loading} size={100} />
+                </div>
+            )}
             {user && <h1>Welcome {user.firstName} {user.lastName}</h1>}
-
-            <CreatePostForm></CreatePostForm>
-            
-        </>
-        <div>
-            {/* Render posts */}
-            {posts && posts.map(post => (
-                <div key={post.id} >
-                    {/* Render each post */}
-                    <Post post={post}/>
-                </div>
-            ))}
-            {!friends && 
-                <div>
-                    gian
-                </div>
-            }
-        </div>
-        
+            <CreatePostForm />
+            <div>
+                {/* Render posts */}
+                {loading ? (
+                    <div className="flex items-center justify-center min-h-screen">
+                        <ClipLoader color="#00008B" loading={loading} size={100} />
+                    </div>
+                ) : (
+                    posts.map(post => (
+                        <div key={post.id}>
+                            <Post post={post} />
+                        </div>
+                    ))
+                )}
+            </div>
         </>
     );
 };
 
 export default Home;
+
+
+
+
 
 
 
